86e19593b63baaa7367ea988e98a19d4b78da39d
AshwiniA-22 <ashwiniavhad229@gmail.com>
2024-05-24 15:08:35 +0530

    Count of Vowels and Consonants

---
+++ src/com/TS/MyPrograms/FindcountVowelsConsonants.java
@@ -0,0 +1,27 @@
+package com.TS.MyPrograms;
+
+public class FindcountVowelsConsonants {
+	
+	public static void main(String[] args) {
+		
+		
+		String s="Maharashtra";
+		
+		char[] ch=s.toCharArray();
+		System.out.println(ch.length);
+		 int vowelCount=0;
+		  int consonantCount=0;
+		  for (int i = 0; i < ch.length; i++) {
+			 
+		 if(ch[i]=='a' || ch[i]=='e' || ch[i]=='i' || ch[i]=='0' || ch[i]=='u') {
+			 vowelCount++;
+		 }else {
+			 consonantCount++;
+		}
+		  }
+		  System.out.println("vowels counts :" +vowelCount);
+		  System.out.println("vowels counts :" + consonantCount);
+		 
+	}
+
+}
